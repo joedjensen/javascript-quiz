68756e2ef23d021c1b1c60e4d53d35fe366af758
josephjensen <joe@cryptonumerics.com>
2022-10-03 22:11:09 -0700

    update questions, finalize styling, last bit of functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,60 @@
+// two elements in HTML
 var headerEl = document.getElementById('page-header')
 var mainEl = document.getElementById('main')
+// set index to 0 at load
 var currentIndex = 0
+// array of question objects to loop through
 var questionArray = [
     {
-        "questionText": "this is the first question",
-        "answerArray": ["wrong", "right", "wrong", "wrong"],
+        "questionText": "Javascript is an _______ language?",
+        "answerArray": ["Object-Oriented", "Object-based", "Procedural", "None of the above"],
+        "correctIndex": 0
+    },
+    {
+        "questionText": "Which of the following keywords is used to define a variable in Javascript?",
+        "answerArray": ["var", "let", "Both A and B", "None of the above"],
+        "correctIndex": 2
+    },
+    {
+        "questionText": "Which of the following methods can be used to display data in some form using Javascript?",
+        "answerArray": ["document.write()","console.log()","window.alert()","None of the above"],
         "correctIndex": 1
     },
     {
-        "questionText": "this is the first question",
-        "answerArray": ["wrong", "wrong", "wrong", "right"],
-        "correctIndex": 3
-    }]
+        "questionText": "Which of the following methods is used to access HTML elements using Javascript?",
+        "answerArray": ["getElementById()","getElementsByClassName()","Both A and B", "None of the above"],
+        "correctIndex": 2
+    },
+    {
+        "questionText": "Which function is used to serialize an object into a JSON string in Javascript?",
+        "answerArray": ["stringify()","parse()","convert()", "None of the above"],
+        "correctIndex": 0
+    },
+    {
+        "questionText": "How do you stop an interval timer in Javascript?",
+        "answerArray": ["clearInterval()","clearTimer()","intervalOver()", "None of the above"],
+        "correctIndex": 0
+    },
+    {
+        "questionText": "Inside which HTML element do we put the Javascript?",
+        "answerArray": ["<js>","<script>","<javascript>", "<scripting>"],
+        "correctIndex": 1
+    },
+    {
+        "questionText": "Which of the following type of variable takes precedence over hte other if names are the same?",
+        "answerArray": ["Local variable","global variable","Both of the above", "None of the above"],
+        "correctIndex": 0
+    },
+    {
+        "questionText": "Javascript is the scripting language of Java?",
+        "answerArray": ["True", "False"],
+        "correctIndex": 1
+    },
+]
+// variable used to store previous answer
+var previousAnswer;
 
+// render home page on page load
 renderHomePage();
 
 function renderHomePage() {
@@ -20,54 +62,62 @@ function renderHomePage() {
     clearEls();
     // reset question index to beginning
     currentIndex = 0;
-    // home page header
+    // anchor link to view high scores
     var highScoreLink = document.createElement('a');
     highScoreLink.textContent = "View Highscores"
     highScoreLink.addEventListener('click', function () {
         renderHighScorePage()
     })
     headerEl.appendChild(highScoreLink)
+    // header element to add to main
     var homePageHeader = document.createElement("h2")
-    homePageHeader.textContent = "JavaScript Quiz"
+    homePageHeader.textContent = "Coding Quiz Challenge"
     mainEl.appendChild(homePageHeader)
     // home page body
     var homePageBody = document.createElement('p')
-    homePageBody.textContent = "Welcome to an interactive JavaScript Quiz. Answer as quickly as you can and see if you can get a high score!"
+    homePageBody.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!"
     mainEl.appendChild(homePageBody);
     // start button
     var startButton = document.createElement("button")
     startButton.textContent = "Start Quiz"
     mainEl.appendChild(startButton)
-    // render first question on 
+    // start game on click of start button
     startButton.addEventListener('click', startGame);
 }
 
+// clear inner HTML for our base elements
 function clearEls() {
     headerEl.innerHTML = '';
     mainEl.innerHTML = '';
 };
 
+// game
 function startGame() {
-    var time = 10;
+    // default time set to 60seconds
+    var time = 60;
     clearEls()
+    // renders timer and high score link
     renderGameHeader()
+    // get span to write time to 
     var timeEl = document.getElementById('time')
     timeEl.textContent = time
+    // timer function
     const timerId = setInterval(function () {
         time--
         timeEl.textContent = time
+        // loss conditiion
         if (time <= 0) {
             // stop timer function
             clearInterval(timerId);
-            // increment losses, store, update text
+            // endGame
             endGame();
         }
     }, 1000)
-
+    // render the first question
     renderNextQuestion();
 
     function renderNextQuestion() {
-        // win condition
+        // win condition, you have finished the questions
         if (currentIndex >= questionArray.length) {
             clearInterval(timerId)
             endGame()
@@ -75,12 +125,16 @@ function startGame() {
         }
         mainEl.innerHTML = '';
         var question = questionArray[currentIndex]
-        var questionBodyEl = document.createElement('p')
+        // get body of question
+        var questionBodyEl = document.createElement('h2')
+        questionBodyEl.classList.add("left-align")
         questionBodyEl.textContent = question.questionText
-        var answersEl = document.createElement('ol')
+        var answersEl = document.createElement('div')
+        answersEl.classList.add("answers")
+        // loop through answers generating HTML elements
         for (i = 0; i < question.answerArray.length; i++) {
             var answer = document.createElement('button')
-            answer.textContent = question.answerArray[i]
+            answer.textContent = (i+1) + ". " + question.answerArray[i]
             if (i == question.correctIndex) {
                 answer.setAttribute("data-correct-answer", true)
             }
@@ -89,16 +143,32 @@ function startGame() {
         answersEl.addEventListener("click", checkAnswer)
         mainEl.appendChild(questionBodyEl)
         mainEl.appendChild(answersEl)
+        // add previous answer element if we are not on the first question
+        if (currentIndex>0) {
+            var previousAnswerEl = document.createElement('p')
+            previousAnswerEl.classList.add('previous-answer')
+            previousAnswerEl.classList.add('show')
+            previousAnswerEl.textContent = previousAnswer
+            // set display to none after 1 second
+            setTimeout(function() {
+                previousAnswerEl.classList.remove('show');
+            }, 1000)
+            mainEl.appendChild(previousAnswerEl)
+        }
+        // iterate index
         currentIndex++
     }
 
-
+    // takes us to the high score submission page
     function endGame() {
         mainEl.innerHTML = ''
+        timeEl.textContent = time
         var endGameHeaderEl = document.createElement("h2")
+        endGameHeaderEl.classList.add("left-align")
         endGameHeaderEl.textContent = "All Done!"
         mainEl.appendChild(endGameHeaderEl)
         var endGameBodyEl = document.createElement('p')
+        endGameBodyEl.classList.add("left-align")
         endGameBodyEl.textContent = "Your score was a " + time
         mainEl.appendChild(endGameBodyEl)
         var formEl = document.createElement('form')
@@ -110,23 +180,36 @@ function startGame() {
         formEl.appendChild(label)
         formEl.appendChild(input)
         formEl.appendChild(button)
+        formEl.classList.add("left-align")
         mainEl.appendChild(formEl)
+        // submit high score on submission click
         button.addEventListener("click", submitHighScore)
     }
 
     function submitHighScore(event) {
+        // prevent default behavior
         event.preventDefault()
+        // get high scores from local storage.
+        // array of score objects with initials and score
         var highScores = localStorage.getItem('highScores')
+        // gets the value of the input form
         var inputVal = document.getElementsByTagName('input')[0].value
+        // create a new score object
         var newScoreObj = { "initials": inputVal, "score": time }
+        // if they have entered text
         if (inputVal != '') {
+            // check if high scores already existed
             if (highScores) {
+                // parse old array
                 highScores = JSON.parse(highScores)
+                // add new object
                 highScores.push(newScoreObj)
+                // sort array so high scores first
                 highScores.sort(compareScoreObjects)
             } else {
                 highScores = [newScoreObj]
             }
+            // stringify the array and set to local storage
             localStorage.setItem('highScores', JSON.stringify(highScores))
             renderHighScorePage()
         }
@@ -134,19 +217,25 @@ function startGame() {
 
 
     function checkAnswer(event) {
+        // make sure they clicked a button
         if (event.target.matches('button')) {
+            // correct answers have this attribute
             if (!event.target.hasAttribute('data-correct-answer')) {
-                time = time - 5
+                time = time - 10
+                // used to create previousAnswerEl
+                previousAnswer = "Incorrect!"
+            } else {
+                previousAnswer = "Correct!"
             }
-            console.log("check")
             renderNextQuestion()
         }
-
-
     }
+
+    // renders header and timer
     function renderGameHeader() {
         var highScoreLink = document.createElement('a');
         highScoreLink.textContent = "View Highscores"
+        // view highscores link stops game timer if clicked during game
         highScoreLink.addEventListener('click', function () {
             clearInterval(timerId)
             renderHighScorePage()
@@ -160,6 +249,7 @@ function startGame() {
 
 function renderTimer() {
     var timerEl = document.createElement('p')
+    // create element with span to write time to
     timerEl.innerHTML = "Timer: <span id = time></span>s"
     headerEl.appendChild(timerEl)
 }
@@ -171,24 +261,30 @@ function renderHighScorePage() {
     var highScoreHeader = document.createElement("h2")
     var highScoreList = document.createElement("ol")
     var highScores = localStorage.getItem("highScores")
+    var buttonsDiv = document.createElement('div')
     var homeButton = document.createElement('button')
     var resetScores = document.createElement('button')
     homeButton.textContent = "Go back"
     resetScores.textContent = "Reset scores"
-
     highScoreHeader.textContent = "High Scores"
+    // add list items for each score
+    // different formating for odd and even scores
     if (highScores) {
         var array = JSON.parse(highScores)
         for (i = 0; i < array.length; i++) {
             var li = document.createElement('li')
-            li.textContent = array[i].initials + "-" + array[i].score
+            if (i % 2 == 0){
+                li.classList.add("even-scores")
+            } else {li.classList.add("odd-scores")}
+            li.textContent = array[i].initials + " - " + array[i].score
             highScoreList.appendChild(li)
         }
     }
     mainEl.appendChild(highScoreHeader)
     mainEl.appendChild(highScoreList)
-    mainEl.appendChild(homeButton)
-    mainEl.appendChild(resetScores)
+    mainEl.appendChild(buttonsDiv)
+    buttonsDiv.appendChild(homeButton)
+    buttonsDiv.appendChild(resetScores)
     homeButton.addEventListener('click', renderHomePage)
     resetScores.addEventListener('click', resetHighScoreList)
 }
